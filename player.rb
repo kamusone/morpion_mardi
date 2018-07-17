d718b36724bdc9c747028ed5f200f89b39606372
loundness <ludo.vic@me.com>
2018-07-17 21:33:26 +0200

    redaction du code pour le fichier board_case.rb

---
+++ player.rb
@@ -0,0 +1,11 @@
+class Player #Class player defines a class for the players
+
+attr_accessor :player_name, :symbol
+
+	def initialize(player_name, symbol)
+		@player_name = player_name
+		@symbol = symbol
+	end
+
+	
+end
